8c23a2bb14ca795b094726375517b1466e8db58a
nvonbulow <nick@nvonbulow.com>
2022-12-15 08:39:36 -0600

    Simplify design of PandaAgent

--- panda_agent/agent.py
+++ panda_agent/agent.py
@@ -7,8 +7,6 @@ class PandaAgent:
 
     def __init__(self, panda: Panda):
         self.panda = panda
-        # Queue your thunks you want executed in the PANDA loop here!
-        self.cmdQueue = None
         self.hasStarted = False
         self.isRunning = False
     
@@ -17,24 +15,12 @@ class PandaAgent:
         self.hasStarted = False
         panda = self.panda
 
-        self.cmdQueue = queue.Queue()
-
         @panda.queue_blocking
-        def process_commands():
+        def panda_start():
             print("panda agent started")
             self.isRunning = True
             # revert to the qcow's root snapshot
             panda.revert_sync("root")
-
-            # receive and process command
-            while True:
-                cmd = self.cmdQueue.get(block=True)
-                if cmd is PandaAgent.STOP_PANDA:
-                    break
-                
-                cmd(panda)
-            
-            panda.end_analysis()
         
         print("starting panda agent ")
         panda.run()
@@ -42,24 +28,31 @@ class PandaAgent:
         self.isRunning = False
     
     def stop(self):
-        self._queue_command(PandaAgent.STOP_PANDA)
+        @self.panda.queue_blocking
+        def panda_stop():
+            self.panda.end_analysis()
     
-    def _queue_command(self, msg):
-        if self.cmdQueue is None:
-            raise RuntimeError("PANDA must be running to send commands")
-        self.cmdQueue.put(msg)
+    def _run_function(self, func, block=True, timeout=None):
+        # Since the queued function will be running in another thread, we need
+        # a queue in order to pass the return value back to this thread
+        returnChannel = queue.Queue()
+
+        @self.panda.queue_blocking
+        def panda_queue_wrapper():
+            returnChannel.put(func(self.panda))
+
+        return returnChannel.get(block=block, timeout=timeout)
     
     def run_command(self, cmd):
-        retQueue = queue.Queue()
-
         def panda_run_command(panda: Panda):
             print(f'running command {cmd}')
-            output = panda.run_serial_cmd(cmd)
-            retQueue.put(output)
-        
-        # Send message to queue
-        self._queue_command(panda_run_command)
+            return panda.run_serial_cmd(cmd)
 
-        # Receive message from queue
-        resp = retQueue.get(block=True, timeout=None)
-        return resp
+        return self._run_function(panda_run_command)
+    
+    def revert_to_snapshot(self, snapshot):
+        def panda_revert_snapshot(panda: Panda):
+            print(f'reverting to snapshot {snapshot}')
+            return panda.revert_sync(snapshot)
+        
+        return self._run_function(panda_revert_snapshot)
