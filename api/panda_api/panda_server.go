ff22ae56a903b1608037d4c7f4e86af7249066b0
rhit-brillew <brillew@rose-hulman.edu>
2023-01-06 11:33:54 -0500

    Fixed CORS issues, returns result from commands

--- api/panda_api/panda_server.go
+++ api/panda_api/panda_server.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	controller "github.com/panda-re/panda_studio/internal/panda_controller"
 	"net/http"
+	"time"
 )
 
 type parameters struct {
@@ -14,9 +16,23 @@ type parameters struct {
 	Name     string   `json:"name"`
 }
 
+type responses struct {
+	Response []string `json:"response"`
+}
+
 func main() {
 	router := gin.Default()
-
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	router.POST("/panda", postRecording)
 
 	router.Run("localhost:8080")
@@ -24,19 +40,18 @@ func main() {
 
 func postRecording(c *gin.Context) {
 	var params parameters
+	var response responses
 
 	if err := c.BindJSON(&params); err != nil {
 		return
 	}
 
-	startExecutor(params.Commands)
+	response.Response = startExecutor(params.Commands)
 
-	c.IndentedJSON(http.StatusCreated, params)
-
-	fmt.Println(params.Commands)
+	c.IndentedJSON(http.StatusCreated, response)
 }
 
-func startExecutor(commands []string) {
+func startExecutor(commands []string) []string {
 	ctx := context.Background()
 
 	agent, err := controller.CreateDefaultDockerPandaAgent(ctx)
@@ -55,6 +70,8 @@ func startExecutor(commands []string) {
 		panic(err)
 	}
 
+	var result []string
+
 	for _, cmd := range commands {
 		fmt.Printf("> %s\n", cmd)
 		cmdResult, err := agent.RunCommand(ctx, cmd)
@@ -62,10 +79,8 @@ func startExecutor(commands []string) {
 			panic(err)
 		}
 		fmt.Printf("%s\n", cmdResult.Logs)
+		result = append(result, cmdResult.Logs+"\n")
 	}
 
-	err = agent.StopAgent(ctx)
-	if err != nil {
-		panic(err)
-	}
+	return result
 }
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
