ab0a6ec47fed686f920b8ac52c6a1fd756d990a4
rhit-brillew <brillew@rose-hulman.edu>
2023-01-12 20:57:23 -0500

    Minor work on image related pages. ImagesPage needs cleanup and the ImageDetails will require some more research.

---
+++ frontend/src/pages/ImageDetails.tsx
@@ -0,0 +1,13 @@
+import {EuiButton, EuiPageTemplate, EuiText} from '@elastic/eui';
+import {MutableRefObject, Ref, useRef, useState} from "react";
+import {EuiFieldText, EuiFlexGroup, EuiFlexItem} from '@elastic/eui';
+import {EuiInnerText} from "@elastic/eui";
+
+function CreateImageDetailsPage() {
+  return(<>
+    <EuiPageTemplate.Header pageTitle="Image Details" />
+  </>)
+}
+
+
+export default CreateImageDetailsPage;
--- frontend/src/pages/ImagesPage.tsx
+++ frontend/src/pages/ImagesPage.tsx
@@ -1,12 +1,45 @@
-import { EuiPageTemplate } from '@elastic/eui';
+import {EuiButton, EuiFieldText, EuiFlexGroup, EuiFlexItem, EuiPageTemplate, EuiText} from '@elastic/eui';
 import ImagesDataGrid from '../components/ImagesDataGrid';
 
 function ImagesPage() {
 
   return (<>
-    <EuiPageTemplate.Header pageTitle="Images" />
+    <EuiPageTemplate.Header pageTitle="Image Dashboard" />
 
     <EuiPageTemplate.Section>
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiText>Image ID</EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiText>Image Name</EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiText>Date</EuiText>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+
+      <EuiFlexGroup>
+        <EuiFlexItem>
+          <EuiFieldText
+            placeholder="Enter Image ID"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiFieldText
+            placeholder="Enter Image Name"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiFieldText
+            placeholder="Date"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem >
+          <EuiButton>Upload Base Image</EuiButton>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+
       <ImagesDataGrid />
     </EuiPageTemplate.Section>
   </>)
