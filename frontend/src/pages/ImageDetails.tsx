05710b77c38cc13dd21ff64369cf9063d79cb277
rhit-brillew <brillew@rose-hulman.edu>
2023-01-22 00:04:07 -0500

    UI work mostly complete, expecting revisions later

--- frontend/src/pages/ImageDetails.tsx
+++ frontend/src/pages/ImageDetails.tsx
@@ -1,4 +1,4 @@
-import {EuiButton, EuiPageTemplate, EuiText} from '@elastic/eui';
+import {EuiButton, EuiFlexGrid, EuiPageTemplate, EuiText} from '@elastic/eui';
 import {MutableRefObject, Ref, useRef, useState} from "react";
 import {EuiFieldText, EuiFlexGroup, EuiFlexItem} from '@elastic/eui';
 import {EuiInnerText} from "@elastic/eui";
@@ -6,55 +6,74 @@ import {useLocation} from "react-router";
 
 function CreateImageDetailsPage() {
   const location = useLocation()
-  console.log(location.state)
+
+  const buttonStyle = {
+    marginRight: "25px",
+    marginTop: "25px"
+  }
 
   return(<>
     <EuiPageTemplate.Header pageTitle="Image Details" />
 
-    <EuiPageTemplate.Section>
-      <EuiText>
-        <strong>ID</strong>
-      </EuiText>
-      <EuiText textAlign={"center"}>
-        {location.state.item.id}
-      </EuiText>
-    </EuiPageTemplate.Section>
+    <EuiFlexGroup>
+      <EuiFlexItem grow={6}>
+        <EuiPageTemplate.Section>
+          <EuiText textAlign={"center"}>
+            <strong>ID:</strong>
+          </EuiText>
+          <EuiText textAlign={"center"}>
+            {location.state.item.id}
+          </EuiText>
+        </EuiPageTemplate.Section>
 
-    <EuiPageTemplate.Section>
-      <EuiText>
-        <strong>Name</strong>
-      </EuiText>
-      <EuiText textAlign={"center"}>
-        {location.state.item.name}
-      </EuiText>
-    </EuiPageTemplate.Section>
+        <EuiPageTemplate.Section>
+          <EuiText textAlign={"center"}>
+            <strong>Name:</strong>
+          </EuiText>
+          <EuiText textAlign={"center"}>
+            {location.state.item.name}
+          </EuiText>
+        </EuiPageTemplate.Section>
 
-    <EuiPageTemplate.Section>
-      <EuiText>
-        <strong>Operating System</strong>
-      </EuiText>
-      <EuiText textAlign={"center"}>
-        {location.state.item.operatingSystem}
-      </EuiText>
-    </EuiPageTemplate.Section>
+        <EuiPageTemplate.Section>
+          <EuiText textAlign={"center"}>
+            <strong>Operating System:</strong>
+          </EuiText>
+          <EuiText textAlign={"center"}>
+            {location.state.item.operatingSystem}
+          </EuiText>
+        </EuiPageTemplate.Section>
 
-    <EuiPageTemplate.Section>
-      <EuiText>
-        <strong>Date Created</strong>
-      </EuiText>
-      <EuiText textAlign={"center"}>
-        {location.state.item.date.toString()}
-      </EuiText>
-    </EuiPageTemplate.Section>
+        <EuiPageTemplate.Section>
+          <EuiText textAlign={"center"}>
+            <strong>Date Created:</strong>
+          </EuiText>
+          <EuiText textAlign={"center"}>
+            {location.state.item.date.toString()}
+          </EuiText>
+        </EuiPageTemplate.Section>
 
-    <EuiPageTemplate.Section>
-      <EuiText>
-        <strong>Size</strong>
-      </EuiText>
-      <EuiText textAlign={"center"}>
-        {location.state.item.size}
-      </EuiText>
-    </EuiPageTemplate.Section>
+        <EuiPageTemplate.Section>
+          <EuiText textAlign={"center"}>
+            <strong>Size:</strong>
+          </EuiText>
+          <EuiText textAlign={"center"}>
+            {location.state.item.size}
+          </EuiText>
+        </EuiPageTemplate.Section>
+      </EuiFlexItem>
+
+      <EuiFlexItem>
+        <EuiFlexGroup direction={"column"}>
+          <EuiFlexItem grow={false}>
+            <EuiButton style={buttonStyle}>Derive New Image</EuiButton>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButton style={buttonStyle}>Delete Image</EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexItem>
+    </EuiFlexGroup>
 
   </>)
 }
