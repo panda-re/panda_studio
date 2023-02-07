204e39f6e36f86e6ea751e03aff496f524c55267
nvonbulow <nick@nvonbulow.com>
2023-02-07 15:23:34 -0600

    I hate it but serialization works well enough now...

--- internal/db/models/programs.go
+++ internal/db/models/programs.go
@@ -1,11 +1,7 @@
 package models
 
 import (
-	"encoding/json"
-	"errors"
-
 	"github.com/panda-re/panda_studio/internal/db"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 type InteractionProgramInstructionList []InteractionProgramInstruction
@@ -24,7 +20,7 @@ type RunCommandInstruction struct {
 	Command string `bson:"command" json:"command"`
 }
 
-func (*RunCommandInstruction) GetInstructionType() string {
+func (RunCommandInstruction) GetInstructionType() string {
 	return "command"
 }
 
@@ -32,80 +28,14 @@ type StartRecordingInstruction struct {
 	RecordingName string `bson:"recording_name" json:"recording_name"`
 }
 
-func (*StartRecordingInstruction) GetInstructionType() string {
+func (StartRecordingInstruction) GetInstructionType() string {
 	return "start_recording"
 }
 
-type MarshalFunc func(interface{}) ([]byte, error)
-type UnmarshalFunc func([]byte, interface{}) error
+type StopRecordingInstruction struct {
 
-type discriminatedInstruction struct {
-	Type string `bson:"type" json:"type"`
-	InteractionProgramInstruction `bson:"args" json:"args"`
 }
 
-func (ip *InteractionProgramInstructionList) marshal(Marshal MarshalFunc) ([]byte, error) {
-	typedInstructions := make([]interface{}, len(*ip))
-	for i, inst := range *ip {
-		typedInstructions[i] = discriminatedInstruction{
-			Type: inst.GetInstructionType(),
-			InteractionProgramInstruction: inst,
-		}
-	}
-
-	return Marshal(typedInstructions)
-}
-
-func (ip *InteractionProgramInstructionList) unmarshal(data []byte, Unmarshal UnmarshalFunc) error {
-	var types []struct { Type string `json:"type" bson:"type"` }
-	err := Unmarshal(data, &types)
-	if err != nil {
-		return err
-	}
-
-	// todo: this needs to be generic to json and bson
-	var rawMessages []json.RawMessage
-	err = Unmarshal(data, &rawMessages)
-	if err != nil {
-		return err
-	}
-
-	instructions := make(InteractionProgramInstructionList, len(types))
-
-	for i, msg := range rawMessages {
-		item := &instructions[i]
-		switch types[i].Type {
-		case "command":
-			*item = &RunCommandInstruction{}
-		case "start_recording":
-			*item = &StartRecordingInstruction{}
-		default:
-			return errors.New("invalid type")
-		}
-		
-		err = Unmarshal(msg, item)
-		if err != nil {
-			return err
-		}
-
-	}
-
-	*ip = instructions
-	return nil
-}
-
-func (ip *InteractionProgramInstructionList) MarshalJSON() ([]byte, error) {
-	return ip.marshal(json.Marshal)
-}
-
-func (ip *InteractionProgramInstructionList) MarshalBSON() ([]byte, error) {
-	return ip.marshal(bson.Marshal)
-}
-
-func (ip *InteractionProgramInstructionList) UnmarshalJSON(data []byte) error {
-	return ip.unmarshal(data, json.Unmarshal)
-}
-
-func (ip *InteractionProgramInstructionList) UnmarshalBSON(data []byte) (error) {
-	return ip.unmarshal(data, bson.Unmarshal)
+func (StopRecordingInstruction) GetInstructionType() string {
+	return "stop_recording"
 }
