dc21198d82a647998eaab1dd65d632bd4f6f5726
rhit-brillew <brillew@rose-hulman.edu>
2023-02-15 11:34:17 -0500

    Added configuration to resolve CORS issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        ws: true,
+      },
+    },
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
