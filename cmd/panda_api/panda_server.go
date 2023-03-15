7e80bb228008046f88cec2a1a41a2c40d236dd85
rhit-brillew <brillew@rose-hulman.edu>
2023-03-15 10:41:22 -0400

    Parsing for new interaction programs, tested and working

--- cmd/panda_api/panda_server.go
+++ cmd/panda_api/panda_server.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"encoding/json"
 	"fmt"
+	config "github.com/panda-re/panda_studio/internal/configuration"
+	"github.com/pkg/errors"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/panda-re/panda_studio/internal/api"
-	config "github.com/panda-re/panda_studio/internal/configuration"
 	"github.com/panda-re/panda_studio/internal/db/models"
 	"github.com/panda-re/panda_studio/internal/middleware"
 	controller "github.com/panda-re/panda_studio/internal/panda_controller"
 )
+
+//go:embed exampleInteractions.txt
+var exampleInteractions string
 
 type parameters struct {
 	Volume   string `json:"volume"`
@@ -111,28 +116,30 @@ func startExecutor(serialized_json string) ([]string, *controller.PandaAgentReco
 	for _, interactions := range programs {
 		fmt.Printf(" %s\n", interactions)
 		instructionList := interactions.Instructions
-		commandArray := strings.Split(instructionList, "\n")
-		for _, cmd := range commandArray {
+		instructions, err := parseProgram(instructionList)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, cmd := range instructions {
 			// Check Type of command and then execute backend as needed for that command.
-			if cmd != "" {
-				instArray := strings.SplitN(cmd, " ", 2)
-				fmt.Println(instArray)
-				switch instArray[0] {
-				case "START_RECORDING":
+			if cmd != nil {
+				switch cmd.GetInstructionType() {
+				case "start_recording":
 					// Since we have a start recording command, we have to type cast cmd to a pointer for a StartRecordingInstruction from the models package
-					err := agent.StartRecording(ctx, instArray[1])
+					err := agent.StartRecording(ctx, cmd.(*models.StartRecordingInstruction).RecordingName)
 					if err != nil {
 						panic(err)
 					}
 					break
-				case "STOP_RECORDING":
+				case "stop_recording":
 					recording, err = agent.StopRecording(ctx)
 					if err != nil {
 						panic(err)
 					}
 					break
-				case "CMD":
-					cmdResult, err := agent.RunCommand(ctx, instArray[1])
+				case "command":
+					cmdResult, err := agent.RunCommand(ctx, cmd.(*models.RunCommandInstruction).Command)
 					if err != nil {
 						panic(err)
 					}
@@ -142,12 +149,20 @@ func startExecutor(serialized_json string) ([]string, *controller.PandaAgentReco
 				case "filesystem":
 					fmt.Printf("Filesystem placeholder\n")
 					break
+				case "network":
+					fmt.Printf("Network Placeholder\n")
+					fmt.Printf("%s\n", cmd.(*models.NetworkInstruction).SocketType)
+					fmt.Printf("%d\n", cmd.(*models.NetworkInstruction).Port)
+					fmt.Printf("%s\n", cmd.(*models.NetworkInstruction).PacketType)
+					fmt.Printf("%s\n", cmd.(*models.NetworkInstruction).PacketData)
+					break
 				default:
 					fmt.Printf("Incorrect Command Type, Correct options can be found in the commands.md file")
 					break
 				}
 			}
 		}
+
 	}
 
 	err = agent.StopAgent(ctx)
@@ -156,4 +171,48 @@ func startExecutor(serialized_json string) ([]string, *controller.PandaAgentReco
 	}
 
 	return result, recording
+}
+
+func parseProgram(instructionList string) (models.InteractionProgramInstructionList, error) {
+	var interactionProgramInstructionList models.InteractionProgramInstructionList
+	commandArray := strings.Split(instructionList, "\n")
+	for _, cmd := range commandArray {
+		res, err := parseInstruction(cmd)
+		if err != nil {
+			return nil, err
+		}
+		if res != nil {
+			interactionProgramInstructionList = append(interactionProgramInstructionList, res)
+		}
+	}
+	return interactionProgramInstructionList, nil
+}
+
+func parseInstruction(cmd string) (models.InteractionProgramInstruction, error) {
+	if cmd != "" {
+		cmd := strings.TrimSpace(cmd)
+		if strings.HasPrefix(cmd, "#") {
+			return nil, nil
+		}
+		instArray := strings.SplitN(cmd, " ", 2)
+		switch instArray[0] {
+		case "START_RECORDING":
+			return &models.StartRecordingInstruction{RecordingName: instArray[1]}, nil
+		case "STOP_RECORDING":
+			return &models.StopRecordingInstruction{}, nil
+		case "CMD":
+			return &models.RunCommandInstruction{Command: instArray[1]}, nil
+		case "filesystem":
+			fmt.Printf("Filesystem placeholder\n")
+			return nil, errors.New("Filesystem interactions not yet supported")
+		case "network":
+			fmt.Printf("Network placeholder\n")
+			return nil, errors.New("Network interactions not yet supported")
+		default:
+			fmt.Printf("Incorrect Command Type, Correct options can be found in the commands.md file")
+			return nil, errors.New("Incorrect Command Type, Correct options can be found in the commands.md file")
+		}
+	}
+
+	return nil, nil
 }
