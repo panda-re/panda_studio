7bc7acd8d54f3dfe10131a45e235819a5e26d325
Steve Finn <finnss@rose-hulman.edu>
2023-03-15 15:43:13 -0400

    cleaned up top navigation bar

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -44,32 +44,10 @@ function Layout() {
               <EuiHeaderLogo iconType={pandaLogo}>PANDA Studio</EuiHeaderLogo>,
               <EuiHeaderLinks aria-label="App navigation dark theme example">
                 <EuiHeaderLink isActive>Docs</EuiHeaderLink>
-                <EuiHeaderLink>Code</EuiHeaderLink>
-                <EuiHeaderLink iconType="help"> Help</EuiHeaderLink>
+                <EuiHeaderLink isActive href='https://github.com/panda-re/panda_studio'>Code</EuiHeaderLink>
               </EuiHeaderLinks>,
             ],
             borders: 'right',
-          },
-          {
-            items: [
-              <EuiBadge
-                color={euiTheme.colors.darkestShade}
-                iconType="arrowDown"
-                iconSide="right"
-              >
-                Production logs
-              </EuiBadge>,
-              <EuiHeaderSectionItemButton
-                aria-label="2 Notifications"
-                notification={'2'}
-              >
-                <EuiIcon type="cheer" size="m" />
-              </EuiHeaderSectionItemButton>,
-              <EuiHeaderSectionItemButton aria-label="Account menu">
-                <EuiAvatar name="John Username" size="s" />
-              </EuiHeaderSectionItemButton>,
-            ],
-            borders: 'none',
           },
         ]}
       />
