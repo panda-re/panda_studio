2b7e12850111f89cd544df06700e814c191e6a34
Steve Finn <finnss@rose-hulman.edu>
2023-03-22 13:30:00 -0400

    fixed create recording to not use hard coded programs

--- frontend/src/pages/CreateRecording.tsx
+++ frontend/src/pages/CreateRecording.tsx
@@ -1,43 +1,12 @@
 import {EuiButton, EuiPageTemplate, EuiSelectableOption, EuiText} from '@elastic/eui';
-import {MutableRefObject, Ref, useCallback, useEffect, useRef, useState} from "react";
+import {useRef, useState} from "react";
 import {EuiFieldText, EuiFlexGroup, EuiFlexItem} from '@elastic/eui';
-import React, { Component } from 'react'
+import React from 'react'
 import EntitySearchBar from '../components/EntitySearchBar';
 
 import prettyBytes from 'pretty-bytes';
 import { useNavigate } from 'react-router';
-import { AxiosRequestConfig } from 'axios';
-import { findAllImages, Image, useFindAllImages } from '../api';
-
-// const sendAPICall = useCallback(function () {
-//   setCommands(JSON.parse("[" + program + "]"))
-//   let recordingDetails = {
-//     volume: volume,
-//     commands: commands,
-//     name: name
-//   }
-//   console.log(recordingDetails)
-
-//   fetch('http://127.0.0.1:8080/panda', {
-//     method: 'POST',
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(recordingDetails)
-//   })
-//     .then(response => response.json())
-//     .then(response => displayResponse(response))
-// }, [name, volume, program])
-
-// const displayResponse = useCallback(function (response: any) {
-//   const term:any = terminal.current
-//   for (let i =0; i < response['response'].length; i++) {
-//     term.pushToStdout('panda@panda:~$ ' + commands[i] + '\n')
-//     term.pushToStdout(response['response'][i])
-//   }
-// }, [commands])
+import { useFindAllImages, useFindAllPrograms } from '../api';
 
 function CreateRecordingPage() {
   const navigate = useNavigate();
@@ -47,12 +16,14 @@ function CreateRecordingPage() {
   const [commands, setCommands] = useState('');
   const terminal = useRef(null);
 
+  var programEntities: EuiSelectableOption[] = [];
   var imageEntities: EuiSelectableOption[] = [];
 
-  const {isLoading, error, data} = useFindAllImages();
+  const {isLoading: imagesLoading, error: imagesError, data: images} = useFindAllImages();
+  const {isLoading: programsLoading, error: programError, data: programs} = useFindAllPrograms();
 
-  if(data != null){
-    data.map((r) =>{
+  if(images != null){
+    images.map((r) =>{
       var size = 0;
       if(r.files != null){
         for(var f of r.files){
@@ -65,64 +36,13 @@ function CreateRecordingPage() {
     })
   }
 
-  // TODO, remove after
-  interface InteractionProgram {
-    id: string;
-    name: string;
-    date: Date;
-  };
-
-  function getProgramEntities(){
-    const programs: InteractionProgram[] = [
-      {
-        id: 'program1',
-        name: 'Test Program 1',
-        date: new Date(),
-      },
-      {
-        id: 'program2',
-        name: 'Test Program 2',
-        date: new Date(),
-      },
-      {
-        id: 'program3',
-        name: 'Test Program 3',
-        date: new Date(),
-      },
-      {
-        id: 'program4',
-        name: 'Test Program 4',
-        date: new Date(),
-      },
-      {
-        id: 'program5',
-        name: 'Test Program 5',
-        date: new Date(),
-      },
-      {
-        id: 'program6',
-        name: 'Test Program 6',
-        date: new Date(),
-      },
-      {
-        id: 'program7',
-        name: 'Test Program 7',
-        date: new Date(),
-      },
-      {
-        id: 'program8',
-        name: 'Test Program 8',
-        date: new Date(),
-      },
-    ]
-
-    // Generate selectable options for Interaction Program search component
-    let interactionProgramEntities: EuiSelectableOption[] = [];
+  // Generate selectable options for Interaction Program search component
+  if(programs != null){
     programs.map((r) =>
-      interactionProgramEntities.push({label: `${r.id} - ${r.name} - ${r.date.toLocaleDateString()}`})
+      programEntities.push({label: `Program Name: ${r.name} ------  Id: ${r.id}`, data: r})
     );
-    return interactionProgramEntities;
   }
+
 
   const [selectedImage, setSelectedImage] = React.useState<EuiSelectableOption | undefined>(undefined);
   function returnSelectedImage(message: EuiSelectableOption){
@@ -158,7 +78,7 @@ function CreateRecordingPage() {
           <EuiText>Image: </EuiText>
         </EuiFlexItem>
         <EuiFlexItem grow={8}>
-          {isLoading && <div>Loading...</div> ||
+          {imagesLoading && <div>Loading...</div> ||
           <EntitySearchBar name="Image" entities={imageEntities} returnSelectedOption={(returnSelectedImage)}></EntitySearchBar>}
         </EuiFlexItem>
       </EuiFlexGroup>
@@ -167,10 +87,11 @@ function CreateRecordingPage() {
     <EuiPageTemplate.Section>
       <EuiFlexGroup justifyContent={"spaceAround"}>
         <EuiFlexItem grow={2}>
-          <EuiText>Specify commands to run:</EuiText>
+          <EuiText>Interaction Program:</EuiText>
         </EuiFlexItem>
         <EuiFlexItem grow={8}>
-          <EntitySearchBar name="Interaction Program" entities={getProgramEntities()} returnSelectedOption={(returnSelectedProgram)}></EntitySearchBar>
+          {programsLoading && <div>Loading...</div> ||
+          <EntitySearchBar name="Interaction Program" entities={programEntities} returnSelectedOption={(returnSelectedProgram)}></EntitySearchBar>}
         </EuiFlexItem>
       </EuiFlexGroup>
     </EuiPageTemplate.Section>
